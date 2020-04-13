1db610186d250b4afb2c3e173ef107cace704a61
Charlie <h.uematsu1015@gmail.com>
2020-04-13 19:11:13 +0900

    Create contiguous-array.js

---
+++ contiguous-array.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/contiguous-array/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+  let map = new Map()
+  let count = 0
+  let max = 0
+  map.set(0, -1)
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] ? 1 : -1
+    if (map.has(count)) {
+      max = Math.max(max, i - map.get(count))
+    } else {
+      map.set(count, i)
+    }
+  }
+  return max
+};

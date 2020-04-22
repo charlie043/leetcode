52083b610fa714aca21f5f623a084327d8346ed0
Charlie <h.uematsu1015@gmail.com>
2020-04-22 19:03:08 +0900

    Create subarray-sum-equals-k.js

---
+++ subarray-sum-equals-k.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+  let sum = 0
+  let hash = new Map()
+  hash.set(0,1)
+  let count = 0
+  
+  for(let num of nums){
+    sum += num
+    if(hash.has(sum - k)){
+      count += hash.get(sum - k)
+    }
+    hash.set(sum, (hash.get(sum) || 0) + 1)
+  }
+    
+  return count
+};

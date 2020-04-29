73f6a41ce47699cddc7de76e90da013107288758
Charlie <h.uematsu1015@gmail.com>
2020-04-29 22:59:18 +0900

    Create binary-tree-maximum-path-sum.js

---
+++ binary-tree-maximum-path-sum.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+  let max = -Number.MAX_VALUE
+  const getMaxSum = (node) => {
+    if (!node) return 0
+    const leftSum = getMaxSum(node.left)
+    const rightSum = getMaxSum(node.right)
+    max = Math.max(max, node.val + leftSum + rightSum)
+    return Math.max(0, node.val + leftSum, node.val + rightSum)
+  }
+  getMaxSum(root)
+  return max
+};

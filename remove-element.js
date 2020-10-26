cc76f77cdd09df6bf973ea5092bf71a60cb4a0ae
Charlie <h.uematsu1015@gmail.com>
2020-10-26 18:48:19 +0900

    Create remove-element.js

---
+++ remove-element.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/remove-element/
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  let len = 0
+  let i = 0
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+    } else {
+      i++
+    }
+  }
+  return nums.length
+};

f6b73cc5a916f9295f51efc94d3a23d39b4dde25
Charlie <h.uematsu1015@gmail.com>
2020-11-16 16:24:13 +0900

    Create design-parking-system.js

---
+++ design-parking-system.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/design-parking-system/
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+  this.parking = [0, big, medium, small]
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+  if (this.parking[carType] > 0) {
+    this.parking[carType]--
+    return true
+  }
+  return false
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */

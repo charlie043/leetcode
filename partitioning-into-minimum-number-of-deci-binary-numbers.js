8d10f0227195f05519d8464a0525f35cb4d8efd2
Charlie <h.uematsu1015@gmail.com>
2020-12-23 15:58:02 +0900

    Create partitioning-into-minimum-number-of-deci-binary-numbers.js

---
+++ partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function(n) {
+  return Math.max(...n.split('').map((n) => parseInt(n)))
+};
